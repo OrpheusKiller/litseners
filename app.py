ceffc5029a5a01cbe6812693ef196d94a8155e57
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 16:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def start():
-    return "The MBSA Server is Running"
-
-@app.route("/mbsa")
-def mbsa():
+def frontend():
     return render_template('index.html')
-
