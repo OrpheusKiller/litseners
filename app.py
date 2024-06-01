ac5b02936ca0cbaba582a345d606f6a40244fa40
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 17:44:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from pyautogui import locateOnScreen, click, center
+# from pyautogui import locateOnScreen, click, center
 from flask import Flask, render_template
-from webbrowser import open
-from time import sleep
+# from webbrowser import open
+# from time import sleep
 
 app = Flask(__name__)
 
