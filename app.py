57a1053cc8682f13a48a6d85d3ef4bc2c7d6b2c2
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 14:28:16 +0530

    main
    main

--- app.py
+++ app.py
@@ -1,40 +1,10 @@
-
+from pyautogui import locateOnScreen, click, center
+from webbrowser import open
 from flask import Flask
+from time import sleep
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    open("https://www.youtube.com/watch?v=oWsnhdwJ_Vw")
-
-    while True:
-        try:
-            locateOnScreen("static\\logo.png", confidence=0.9)
-            break
-        except:
-            continue
-
-    try:
-        if locateOnScreen("static\\subscribe.png", confidence=0.9):
-            click(center(locateOnScreen("static\\subscribe.png", confidence=0.9)))
-    except:
-            pass
-
-    try:
-        if locateOnScreen("static\\like.png", confidence=0.9):
-            click(center(locateOnScreen("static\\like.png", confidence=0.9)))
-    except:
-            pass
-    try:
-        if locateOnScreen("static\\full.png", confidence=0.9):
-            click(center(locateOnScreen("static\\full.png", confidence=0.9)))
-    except:
-            pass
-
-    try:
-        if locateOnScreen("static\\lpt.png", confidence=0.9):
-            click(center(locateOnScreen("static\\lpt.png", confidence=0.9)))
-    except:
-            pass
-    
-    return ''
+    return 'hello world'
