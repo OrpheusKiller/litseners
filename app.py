cb784e4fc617eb2bd80b3e0a647d79332ecac0d2
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 13:56:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,37 +7,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    open("https://www.youtube.com/watch?v=oWsnhdwJ_Vw")
-
-    while True:
-        try:
-            locateOnScreen("static\\logo.png", confidence=0.9)
-            break
-        except:
-            sleep(5)
-            continue
-
-    try:
-        if locateOnScreen("static\\subscribe.png", confidence=0.9):
-            click(center(locateOnScreen("static\\subscribe.png", confidence=0.9)))
-    except:
-            pass
-
-    try:
-        if locateOnScreen("static\\like.png", confidence=0.9):
-            click(center(locateOnScreen("static\\like.png", confidence=0.9)))
-    except:
-            pass
-    try:
-        if locateOnScreen("static\\full.png", confidence=0.9):
-            click(center(locateOnScreen("static\\full.png", confidence=0.9)))
-    except:
-            pass
-
-    try:
-        if locateOnScreen("static\\lpt.png", confidence=0.9):
-            click(center(locateOnScreen("static\\lpt.png", confidence=0.9)))
-    except:
-            pass
-        
     return ''
