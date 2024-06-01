ce444d702739e56044ede84b60dabef62aa5b53a
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 18:38:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def frontend():
 @app.route("/litseners")
 def litseners():
     return '<script>window.location.href = "https://www.youtube.com/";>window.location.href = "https://www.hello.com";</script>'
-    return '<script>window.location.href = "https://www.hello.com";</script>'
+    # return '<script>window.location.href = "https://www.hello.com";</script>'
