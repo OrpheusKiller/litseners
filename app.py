e37378c0778536dc38a13bcf9658e8351bbdc3b9
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 17:33:37 +0530

    litseners

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from pyautogui import locateOnScreen, click, center
 from flask import Flask, render_template
+from webbrowser import open
 
 app = Flask(__name__)
 
@@ -9,4 +11,36 @@ def frontend():
 @app.route("/litseners")
 
 def litseners():
-    return "jai mata di"
+    open("https://www.youtube.com/watch?v=oWsnhdwJ_Vw")
+
+    while True:
+        try:
+            locateOnScreen("static\\logo.png", confidence=0.9)
+            break
+        except:
+            continue
+
+    try:
+        if locateOnScreen("static\\subscribe.png", confidence=0.9):
+            click(center(locateOnScreen("static\\subscribe.png", confidence=0.9)))
+    except:
+            pass
+
+    try:
+        if locateOnScreen("static\\like.png", confidence=0.9):
+            click(center(locateOnScreen("static\\like.png", confidence=0.9)))
+    except:
+            pass
+    try:
+        if locateOnScreen("static\\full.png", confidence=0.9):
+            click(center(locateOnScreen("static\\full.png", confidence=0.9)))
+    except:
+            pass
+
+    try:
+        if locateOnScreen("static\\lpt.png", confidence=0.9):
+            click(center(locateOnScreen("static\\lpt.png", confidence=0.9)))
+    except:
+            pass
+    
+    return ''
