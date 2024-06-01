8dcfee1417a17ab5c8151b68c34465b0f3ad2a80
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 18:13:57 +0530

    main

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 # from pyautogui import locateOnScreen, click, center
 from flask import Flask, render_template
 from webbrowser import open
-from time import sleep
 
 app = Flask(__name__)
 
 @app.route("/")
 def frontend():
-    sleep(5)
     return render_template('index.html')
-sleep(5)
-@app.route("/litseners")
 
+@app.route("/litseners")
 def litseners():
-    open("https://www.youtube.com/watch?v=oWsnhdwJ_Vw")
+    return '<script>window.location.href = "";</script>'
