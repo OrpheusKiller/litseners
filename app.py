5bcc57cbb6fd2387727eeaf101674f8275a108f4
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 13:12:35 +0530

    litseners
    litseners

---
+++ app.py
@@ -0,0 +1,42 @@
+from pyautogui import locateOnScreen, click, center
+from webbrowser import open
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    open("https://www.youtube.com/watch?v=oWsnhdwJ_Vw")
+
+    while True:
+        try:
+            locateOnScreen("static\\logo.png", confidence=0.9)
+            break
+        except:
+            continue
+
+    try:
+        if locateOnScreen("static\\subscribe.png", confidence=0.9):
+            click(center(locateOnScreen("static\\subscribe.png", confidence=0.9)))
+    except:
+            pass
+
+    try:
+        if locateOnScreen("static\\like.png", confidence=0.9):
+            click(center(locateOnScreen("static\\like.png", confidence=0.9)))
+    except:
+            pass
+    try:
+        if locateOnScreen("static\\full.png", confidence=0.9):
+            click(center(locateOnScreen("static\\full.png", confidence=0.9)))
+    except:
+            pass
+
+    try:
+        if locateOnScreen("static\\lpt.png", confidence=0.9):
+            click(center(locateOnScreen("static\\lpt.png", confidence=0.9)))
+    except:
+            pass
+    
+    return ''
+
