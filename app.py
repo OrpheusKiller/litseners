a8544c0fa5f4f6552518b853210c6d53f448c737
OrpheusKiller <163430795+OrpheusKiller@users.noreply.github.com>
2024-06-01 17:35:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,36 +11,4 @@ def frontend():
 @app.route("/litseners")
 
 def litseners():
-    open("https://www.youtube.com/watch?v=oWsnhdwJ_Vw")
-
-    while True:
-        try:
-            locateOnScreen("static\\logo.png", confidence=0.9)
-            break
-        except:
-            continue
-
-    try:
-        if locateOnScreen("static\\subscribe.png", confidence=0.9):
-            click(center(locateOnScreen("static\\subscribe.png", confidence=0.9)))
-    except:
-            pass
-
-    try:
-        if locateOnScreen("static\\like.png", confidence=0.9):
-            click(center(locateOnScreen("static\\like.png", confidence=0.9)))
-    except:
-            pass
-    try:
-        if locateOnScreen("static\\full.png", confidence=0.9):
-            click(center(locateOnScreen("static\\full.png", confidence=0.9)))
-    except:
-            pass
-
-    try:
-        if locateOnScreen("static\\lpt.png", confidence=0.9):
-            click(center(locateOnScreen("static\\lpt.png", confidence=0.9)))
-    except:
-            pass
-    
-    return ''
+    return 'jai mata di'
